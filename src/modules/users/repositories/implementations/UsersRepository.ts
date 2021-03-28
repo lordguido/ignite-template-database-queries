0b4836782730232060913536dc2987cdc5e73608
Rodrigo Dematei de Freitas <rodfrei@gmail.com>
2021-03-28 12:20:31 -0300

    test ok

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -11,20 +11,23 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  async findUserWithGamesById(data: IFindUserWithGamesDTO): Promise<User> {
+    return await this.repository.findOneOrFail(data.user_id, {
+      relations: ['games']
+    })
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM USERS ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM USERS WHERE first_name ILike $1 AND last_name ILike $2', [
+      `%${first_name}%`,
+      `%${last_name}%`,
+    ]);
   }
 }
